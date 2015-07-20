fec109c1785ddda14c8c2a7d69dda83c0c4588fe
Erik Rantapaa <erantapaa@gmail.com>
2015-07-20 00:02:39 -0500

    Add TarUtil.hs

--- app/Main.hs
+++ app/Main.hs
@@ -7,95 +7,37 @@ import Control.Monad
 import qualified System.IO.Strict as S
 
 import qualified PackageRank as PR
-import qualified Data.Map as M
+import qualified Data.Map.Strict as Map
 import Data.List (isInfixOf)
+import qualified TarUtil as T
+import Pipes
+import qualified Pipes.Prelude as P
 
-main2 :: IO ()
-main2 = L.main
-
--- read and parse all the cabal files
-main3 :: IO ()
-main3 = do
-  paths <- fmap lines getContents
-  forM_ paths $ \path -> do
-    cbl <- readFile path
-    let pair = fmap (\d -> (L.p_name d, L.p_dependencies d)) (L.parseCabal cbl)  
-    case pair of
-      Nothing -> putStrLn $ path ++ ": unable to parse"
-      Just x  -> print x
-
-main4 = do
-  paths <- fmap lines S.getContents
-  mnodes <- forM paths $ \path -> do
-              cbl <- S.readFile path
-              let pair = fmap (\d -> (L.p_name d, L.p_dependencies d)) (L.parseCabal cbl)  
-              return pair
-  let nodes = catMaybes mnodes
-      ranking = PR.rankingStd nodes 
-  forM_ (M.assocs ranking) $ \(k,v) -> do
-    putStrLn $ k ++ " " ++ show v
+import Text.Show.Pretty
+import qualified Data.ByteString.Lazy.Char8 as LBS
 
 safeHead a [] = a
 safeHead _ (a:_) = a
 
-main5 = do
+computeRanks indexTarPath = do
+  entries <- T.tarEntriesForPath indexTarPath
+  cabals <- P.toListM $ T.pipesTarEntries entries >-> T.pipesSelectCabals >-> T.pipesLatestVersions
+  -- create a map of PackageInfo records
+  -- create the map of rankings
+  let pe_to_info = L.parseCabal . LBS.unpack . T.pe_content
+      pkgInfoMap = Map.fromList [ (T.pe_package pe, pinfo) | pe <- cabals, Just pinfo <- [ pe_to_info pe ] ]
+      nodes = [ (pkg, L.p_dependencies pinfo) | (pkg, pinfo) <- Map.assocs pkgInfoMap ]
+      rankingMap = PR.rankingStd nodes
+      pkgInfoMap' = Map.fromList [ (pkg, pinfo') | (pkg, pinfo) <- Map.assocs pkgInfoMap,
+                                      let pinfo' = pinfo { L.p_rank = Map.findWithDefault 0 pkg rankingMap } ]
+  forM_ (Map.assocs pkgInfoMap') $ \(pkg,pinfo) -> do
+    putStrLn $ ppShow pinfo
+
+main6 = do
   args <- getArgs
-  let cmd = safeHead "deps" args
-  case cmd of
-    "deps"  -> showDeps
-    "ranks" -> showRanks
-    "condtar" -> findConduitTar
-    _       -> error $ "unknown command: " ++ cmd
+  case args of
+    (path:_) -> do computeRanks path
+    _        -> error "bad usage"
 
-showDeps = do
-  paths <- fmap lines S.getContents
-  forM_ paths $ \path -> do
-    cbl <- S.readFile path
-    let pair = fmap (\d -> (L.p_name d, L.p_dependencies d)) (L.parseCabal cbl)  
-    case pair of
-      Nothing           -> putStrLn $ path ++ ": unable to parse cabal file"
-      Just (name, deps) -> emitPackage name deps
-
-showRanks = do
-  paths <- fmap lines S.getContents
-  mnodes <- forM paths $ \path -> do
-              cbl <- S.readFile path
-              let pair = fmap (\d -> (L.p_name d, L.p_dependencies d)) (L.parseCabal cbl)  
-              return pair
-  let nodes = catMaybes mnodes
-      ranking = PR.rankingStd nodes 
-  forM_ (M.assocs ranking) $ \(k,v) -> do
-    putStrLn $ k ++ " " ++ show v
-
-findConduitTar = do
-  paths <- fmap lines S.getContents
-  forM_ paths $ \path -> do
-    cbl <- S.readFile path
-    let pair = fmap (\d -> (L.p_name d, L.p_dependencies d)) (L.parseCabal cbl)  
-    case pair of
-      Nothing           -> putStrLn $ path ++ ": unable to parse cabal file"
-      Just (name, deps) -> when (hasConduitAndTar deps) $ do
-                             emitPackage name deps; putStrLn ""
-                             
-hasConduitAndTar deps =
-  any ("conduit" `isInfixOf` ) deps && any ("tar" `isInfixOf`) deps
-
-breakLine maxLength xs@[] = (xs,xs)
-breakLine maxLength xs@(x:xs')
-  | len <= maxLength = let (ys,zs) = breakLine (maxLength-1-len) xs'
-                       in (x:ys,zs)
-  | otherwise        = ([],xs)
-  where len = length x
-
-breakLines maxLength ws =
-  case breakLine maxLength ws of
-    ([], [])     -> []
-    ([], (x:xs)) -> [x] : breakLines maxLength xs
-    (xs, ys)     -> xs : breakLines maxLength ys
-
-emitPackage name deps = do
-  putStrLn $ name ++ ":"
-  forM_ (breakLines 80 deps) $ \ws -> putStrLn $ "  " ++ unwords ws
-
-main = main5
+main = main6
 
--- src/TestTar.hs
+++ src/TestTar.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module TestTar where
 
@@ -11,6 +13,21 @@ import Data.List (isSuffixOf, unfoldr)
 import qualified Data.Version as V
 
 import Text.ParserCombinators.ReadP
+
+import Pipes 
+import qualified Pipes.Prelude as P
+import qualified Pipes.Parse as P
+import Control.Monad
+import Data.Maybe
+import Data.Typeable
+import Control.Exception
+
+import qualified Data.Map.Strict as Map
+
+data TarException = TarFormatError Tar.FormatError
+  deriving (Show, Typeable)
+
+instance Exception TarException where
 
 tarEntriesForPath path = do
   let decompress
@@ -34,8 +51,6 @@ pathParts path = unfoldr go path
                  ([], [])     -> Nothing
                  ([], rest)   -> go (tail rest)
                  (leaf, rest) -> Just (leaf, rest)
-
-
 
 hasThreeParts path =
  case pathParts path of
@@ -62,7 +77,7 @@ listCabalFiles path = do
   entries <- tarEntriesForPath path
   let loop (Tar.Next ent nxt) = do process ent; loop nxt
       loop (Tar.Done)         = return ()
-      loop (Tar.Fail e)       = putStrLn "oops - an error"
+      loop (Tar.Fail e)       = throw (TarFormatError e)
       process ent = let epath = Tar.entryPath ent
                     in print $ parsePath epath
   loop entries
@@ -88,4 +103,114 @@ printLatestCabalFiles path = do
   case x of
     Just y -> putStrLn $ show y
     _      -> return ()
+
+tarEntriesProducer path = do
+  entries <- lift $  tarEntriesForPath path
+  let loop (Tar.Next ent nxt) = do yield ent; loop nxt
+      loop (Tar.Done)         = return ()
+      loop (Tar.Fail e)       = throw (TarFormatError e)
+  loop entries
+
+-- filter a stream of Tar entries selecting only the latest versions
+latestVersions m = do
+  ment <- P.draw
+  case ment of
+    Nothing -> return m
+    Just ent -> do
+      case parsePath (Tar.entryPath ent) of
+        Nothing             -> latestVersions m
+        Just (pkg, vers, _) -> let m' = Map.insertWith max pkg vers m
+                               in latestVersions m'
+
+doit f = forever $ do { x <- await; f x }
+
+pipesListCabalFiles path = runEffect $ for (tarEntriesProducer path) (lift . print . Tar.entryPath)
+
+pipesListLatestCabalFiles path = do
+  latest <- P.evalStateT (latestVersions Map.empty) (tarEntriesProducer path)
+  forM_ (Map.assocs latest) $ \(k,v) -> do
+    putStrLn $ k ++ ": " ++ show v
+
+newPackage Nothing _ = False
+newPackage (Just ((oldPkg, _, _), _)) ((newPkg, _, _),_) = oldPkg /= newPkg
+
+laterVersion Nothing _ = True
+laterVersion old@(Just ((oldPkg, oldVers, _),_)) ((newPkg, newVers, _),_)
+  | oldPkg /= newPkg  = False
+  | oldVers < newVers = True
+  | otherwise         = False
+
+latestVersions2 = do
+  let loop old = do
+        mnew <- P.draw
+        case mnew of
+          Nothing -> return ()
+          Just new  -> if newPackage old new
+                          then do lift $ print $ fst (fromJust old)
+                                  loop (Just new)
+                          else if laterVersion old new
+                                 then loop (Just new)
+                                 else loop old
+  loop Nothing
+
+dropNothings = do
+  x <- await
+  case x of
+    Nothing -> return ()
+    Just y  -> yield y
+  dropNothings
+
+latestVersions3 = do
+  let loop0 = do
+        mnew <- P.draw
+        case mnew of
+          Nothing -> return ()
+          Just old -> loop old
+      newPackage' ((oldPkg,_,_),_) ((newPkg,_,_),_) = oldPkg /= newPkg
+      laterVersion' ((oldPkg,oldVers,_),_) ((newPkg,newVers,_),_)
+        = oldPkg == newPkg && oldVers < newVers
+      showEnt ((pkg,vers,_),content) = 
+        putStrLn $ pkg ++ " length: " ++ show (LBS.length content) ++ " version: " ++ show vers
+      loop old = do
+        mnew <- P.draw
+        case mnew of
+          Nothing -> lift $ showEnt old
+          Just new -> if newPackage' old new
+                        then do lift $ showEnt old
+                                loop new
+                        else if laterVersion' old new
+                               then loop new
+                               else loop old
+  loop0
+
+getContent ent =
+  case Tar.entryContent ent of
+    Tar.NormalFile bs len -> Just bs
+    _                     -> Nothing
+
+pipesParsePath = forever $ do
+  ent <- await
+  let mr = do content <- getContent ent
+              x <- parsePath (Tar.entryPath ent)
+              return (x,content)
+  case mr of
+    Nothing -> return ()
+    Just r  -> yield r
+
+showIt = do x <- await
+            lift $ print (fst x)
+            yield x
+            showIt
+
+devNull = do x <- await; devNull
+
+pipesLatestVersions2 path = do
+  P.evalStateT latestVersions2 (tarEntriesProducer path >-> pipesParsePath)
+  return ()
+
+pipesLatestVersions3 path = do
+  P.evalStateT latestVersions3 (tarEntriesProducer path >-> pipesParsePath)
+  return ()
+
+-- test2 path = runEffect $ tarEntriesProducer path >-> pipesParsePath >-> showIt >-> devNull
 
